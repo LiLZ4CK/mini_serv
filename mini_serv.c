ff79f0bab6533dbeeb9e6ec5940eaa5e945d63df
Zakaria Walad <zwalad@c3r6p6.1337.ma>
2023-11-23 12:11:08 +0100

    first commit

---
+++ mini_serv.c
@@ -0,0 +1,112 @@
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+int max, ids = 0, sockfd = 0;
+fd_set set, rd, wr;
+
+typedef struct clients{
+    int id;
+    char msg[200100];
+}t_clients;
+
+t_clients clients[1024];
+char buff[200100];
+
+
+void fatal_error()
+{
+    write(2, "Fatal error\n", 12);
+    exit(1);
+}
+
+void sendit(int s)
+{
+    for (int fd = 0; fd <= max; fd++){
+        if (fd != s && FD_ISSET(fd, &wr)){
+            send(fd, buff, strlen(buff), 0);
+        }
+    }
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        write(2, "Wrong number of arguments\n", 26);
+        exit(1);
+    }
+    bzero(&clients, sizeof(clients));
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        fatal_error();
+
+    struct sockaddr_in addr, cli;
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(2130706433);
+    addr.sin_port = htons(atoi(av[1]));
+
+    if (bind(sockfd, (const struct sockaddr *) &addr, sizeof(addr)) < 0)
+        fatal_error();
+    if (listen(sockfd, 128) < 0)
+        fatal_error();
+
+    max = sockfd;
+    int connfd;
+    socklen_t len = sizeof(cli);
+    FD_ZERO(&set);
+    FD_SET(sockfd, &set);
+
+    while(1)
+    {
+        rd = wr = set;
+        if (select(max + 1, &rd, &wr, 0, 0) < 0)
+            continue;
+        for (int fd = 0; fd <= max; fd++)
+        {
+            if (FD_ISSET(sockfd, &rd) && fd == sockfd)
+            {
+                connfd = accept(sockfd, (struct sockaddr *) &cli, &len);
+                if (connfd < 0)
+                    continue;
+                FD_SET(connfd, &set);
+                sprintf(buff, "server: client %d just arrived\n", ids);
+                sendit(connfd);
+                clients[connfd].id = ids++;
+                if (connfd > max){
+                    max = connfd;
+                }
+                break;
+            }
+            if (FD_ISSET(fd, &rd) && fd != sockfd)
+            {
+                int r = 1;
+                bzero(&clients[fd].msg, sizeof(clients[fd].msg));
+                while (r == 1 && clients[fd].msg[strlen(clients[fd].msg) - 1] != '\n')
+                {
+                    r = recv(fd, clients[fd].msg + strlen(clients[fd].msg), 1, 0);
+                }
+                if (r < 1)
+                {
+                    sprintf(buff, "server: client %d just left\n", clients[fd].id);
+                    FD_CLR(fd, &set);
+                    close(fd);
+                }
+                else{
+                    sprintf(buff, "client %d: %s", clients[fd].id, clients[fd].msg);
+                }
+                sendit(fd);
+                break;
+            }
+
+        }
+        
+    }
+}
